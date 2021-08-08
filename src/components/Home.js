870519dd399b77ed668e07b8044ae4b5c896a8a7
WalterIran <amadorwalter3418@gmail.com>
2021-08-07 21:52:52 -0600

    Fetching movie data from the api and searchbar functionality ready

---
+++ src/components/Home.js
@@ -0,0 +1,52 @@
+import React from 'react';
+
+//config
+import {POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL} from '../config';
+
+//Components
+import HeroImage from './HeroImage/HeroImage';
+import Grid from './Grid/Grid';
+import Thumb from './Thumb/Thumb';
+import { Spinner } from './Spinner/Spinner.styles';
+import SearchBar from './SearchBar/SearchBar';
+
+//Hook
+import {useHomeFetch} from '../hooks/useHomeFetch';
+
+//Image
+import NoImage from '../images/no_image.jpg';
+
+
+const Home = () =>{
+
+    const {state, loading, error, searchTerm, setSearchTerm} = useHomeFetch();
+
+    return(
+        <>
+            {!searchTerm && state.results[0] && 
+            <HeroImage 
+                image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`} 
+                title={state.results[0].original_title}
+                text={state.results[0].overview}
+            /> }
+            <SearchBar setSearchTerm={setSearchTerm}/>
+            <Grid header='Popular Movies' >
+                {state.results.map(movie => (
+                    <Thumb 
+                        key={movie.id} 
+                        clickable
+                        image={
+                            movie.poster_path ? IMAGE_BASE_URL + POSTER_SIZE + movie.poster_path
+                            : NoImage
+                        }
+                        movieId={movie.id}
+                    />
+                ))}
+            </Grid>
+
+            <Spinner />
+        </>
+    );
+}
+
+export default Home;
